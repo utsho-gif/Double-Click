c511ee756c418fb2128fb90cb503928ac604005f
Iftekhar Utsho <future.slangsta76@gmail.com>
2022-04-18 01:19:06 +0600

    error message rendered

--- src/pages/Login/Login/Login.js
+++ src/pages/Login/Login/Login.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from "../../../firebase.init";
 import SocialLogin from "../SocialLogin/SocialLogin";
+import Loading from '../../Shared/Loading/Loading';
 
 const Login = () => {
     const navigate = useNavigate();
@@ -15,12 +16,29 @@ const Login = () => {
         error,
       ] = useSignInWithEmailAndPassword(auth);
 
+      let loadingEle;
+      if(loading){
+          loadingEle = (
+              <p>
+                  <Loading></Loading>
+              </p>
+          )
+      }
+
+      let errorELe;
+      if(error){
+          errorELe = (
+              <p className="text-danger fw-bold">{error?.message}</p>
+          )
+      }
+
       const handleSignIn = event => {
           event.preventDefault();
           const email = event.target.email.value;
           const password = event.target.password.value;
           signInWithEmailAndPassword(email, password);
       }
+      
       if(user){
           navigate('/home');
       }
@@ -35,6 +53,10 @@ const Login = () => {
         <Form.Group className="mb-5" controlId="formBasicPassword">
           <Form.Control name="password" type="password" placeholder="Password" />
         </Form.Group>
+        {
+            error? errorELe : loadingEle
+        }
+        
         <Button
           className="w-50 mx-auto d-block rounded-pill mb-1"
           variant="primary"
@@ -43,8 +65,8 @@ const Login = () => {
           Sign In
         </Button>
       </Form>
-      <p className="mt-2">
-        New To Genius?{" "}
+      <p className="mt-4">
+        New To Click?{" "}
         <Link className="text-decoration-none text-primary" to={"/signup"}>
           Create An Account
         </Link>
